57f28bc7d51664bbf0ce7cab340c05d7be2494a3
Raka Atmaja <rakapa23@gmail.com>
2023-02-02 01:53:02 +0800

    profile

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -6,9 +6,14 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('Profile'),
-      ),
+      body: Scaffold(
+        body: Column(
+          
+          children: [
+
+          ]
+        ), 
+      )
     );
   }
 }
